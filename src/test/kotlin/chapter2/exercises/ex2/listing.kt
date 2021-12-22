b38bbc73c872cee055f5bbed6db708b1243afbe1
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-22 21:25:38 +0100

    Exercise 2.2

--- src/test/kotlin/chapter2/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter2/exercises/ex2/listing.kt
@@ -3,7 +3,6 @@ package chapter2.exercises.ex2
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import kotlinx.collections.immutable.persistentListOf
-import utils.SOLUTION_HERE
 
 // tag::init[]
 val <T> List<T>.tail: List<T>
@@ -12,40 +11,47 @@ val <T> List<T>.tail: List<T>
 val <T> List<T>.head: T
     get() = first()
 
-fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean =
+fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean {
+    tailrec fun go(list: List<A>): Boolean =
+        when {
+            list.isEmpty() || list.size == 1 -> true
+            !order(list.head, list.tail.head) -> false
+            else -> go(list.tail)
+        }
+    return go(aa)
+}
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise2 : WordSpec({
 
     "isSorted" should {
-        """!detect ordering of a list of correctly ordered Ints based
+        """detect ordering of a list of correctly ordered Ints based
             on an ordering HOF""" {
             isSorted(
                 persistentListOf(1, 2, 3)
             ) { a, b -> b > a } shouldBe true
         }
-        """!detect ordering of a list of incorrectly ordered Ints
+        """detect ordering of a list of incorrectly ordered Ints
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf(1, 3, 2)
             ) { a, b -> b > a } shouldBe false
         }
-        """!verify ordering of a list of correctly ordered Strings
+        """verify ordering of a list of correctly ordered Strings
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf("a", "b", "c")
             ) { a, b -> b > a } shouldBe true
         }
-        """!verify ordering of a list of incorrectly ordered Strings
+        """verify ordering of a list of incorrectly ordered Strings
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf("a", "z", "w")
             ) { a, b -> b > a } shouldBe false
         }
-        "!return true for an empty list" {
+        "return true for an empty list" {
             isSorted(persistentListOf<Int>()) { a, b ->
                 b > a
             } shouldBe true
