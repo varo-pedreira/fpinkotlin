3d366242a8e912663eed7915041f44d544f130d6
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-22 21:45:18 +0100

    Exercise 2.5

--- src/test/kotlin/chapter2/exercises/ex5/listing.kt
+++ src/test/kotlin/chapter2/exercises/ex5/listing.kt
@@ -2,18 +2,17 @@ package chapter2.exercises.ex5
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise5 : WordSpec({
     // tag::init[]
     fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
+        { a -> f(g(a)) }
 
-        SOLUTION_HERE()
     // end::init[]
 
     "compose" should {
-        "!apply function composition over two functions" {
+        "apply function composition over two functions" {
             val fahrenheit2celsius: (Double) -> String =
                 compose<Double, Double, String>(
                     { b -> "$b degrees celsius" },
