2770e282b18ffb1660f1a81cba7f38c7db3998c8
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 17:30:17 +0100

    Exercise 3.19

--- src/test/kotlin/chapter3/exercises/ex19/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex19/listing.kt
@@ -1,20 +1,22 @@
 package chapter3.exercises.ex19
 
 import chapter3.List
+import chapter3.append
+import chapter3.foldLeft
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A, B> flatMap(xa: List<A>, f: (A) -> List<B>): List<B> =
+    foldLeft(xa, List.empty()) { ls, x -> append(ls, f(x)) }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise19 : WordSpec({
     "list flatmap" should {
-        "!map and flatten a list" {
+        "map and flatten a list" {
             val xs = List.of(1, 2, 3)
             flatMap(xs) { i -> List.of(i, i) } shouldBe
                 List.of(1, 1, 2, 2, 3, 3)
