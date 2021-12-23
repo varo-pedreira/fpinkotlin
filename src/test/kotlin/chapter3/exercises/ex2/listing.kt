d8a11b119bdeeb645dd4221f1e13aaa190783cee
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 11:37:38 +0100

    Exercise 3.2

--- src/test/kotlin/chapter3/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex2/listing.kt
@@ -1,27 +1,29 @@
 package chapter3.exercises.ex2
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun <A> setHead(xs: List<A>, x: A): List<A> =
+fun <A> setHead(xs: List<A>, x: A): List<A> = when (xs) {
+    is Cons -> Cons(x, xs.tail)
+    Nil -> throw IllegalStateException()
+}
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise2 : WordSpec({
     "list setHead" should {
-        "!return a new List with a replaced head" {
+        "return a new List with a replaced head" {
             setHead(List.of(1, 2, 3, 4, 5), 6) shouldBe
                 List.of(6, 2, 3, 4, 5)
         }
 
-        "!throw an illegal state exception when no head is present" {
+        "throw an illegal state exception when no head is present" {
             shouldThrow<IllegalStateException> {
                 setHead(Nil, 6)
             }
