1b71a4e4bd4f5fc744adcae13bd7d1ef47db4562
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 17:07:37 +0100

    Exercise 3.16

--- src/test/kotlin/chapter3/exercises/ex16/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex16/listing.kt
@@ -1,20 +1,21 @@
 package chapter3.exercises.ex16
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun doubleToString(xs: List<Double>): List<String> =
+    foldRight(xs, List.empty()) { x, ls -> Cons(x.toString(), ls) }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise16 : WordSpec({
     "list doubleToString" should {
-        "!convert every double element to a string" {
+        "convert every double element to a string" {
             doubleToString(List.of(1.1, 1.2, 1.3, 1.4)) shouldBe
                 List.of("1.1", "1.2", "1.3", "1.4")
         }
