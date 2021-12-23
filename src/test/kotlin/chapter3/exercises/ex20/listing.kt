90bd3a72a65c45396e6964a6402a459d42bdab20
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 17:33:14 +0100

    Exercise 3.20

--- src/test/kotlin/chapter3/exercises/ex20/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex20/listing.kt
@@ -1,20 +1,21 @@
 package chapter3.exercises.ex20
 
 import chapter3.List
+import chapter3.Nil
+import chapter3.flatMap
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> filter2(xa: List<A>, f: (A) -> Boolean): List<A> =
+    flatMap(xa) { if (f(it)) List.of(it) else Nil }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise20 : WordSpec({
     "list filter" should {
-        "!filter out elements not compliant to predicate" {
+        "filter out elements not compliant to predicate" {
             filter2(
                 List.of(1, 2, 3, 4, 5)
             ) { it % 2 == 0 } shouldBe List.of(2, 4)
