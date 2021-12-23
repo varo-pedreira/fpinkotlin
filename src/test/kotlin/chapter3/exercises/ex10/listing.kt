e30faa77770cac82b14d5e2381ca465ef6f058df
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 14:48:47 +0100

    Exercise 3.10

--- src/test/kotlin/chapter3/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex10/listing.kt
@@ -1,40 +1,35 @@
 package chapter3.exercises.ex10
 
 import chapter3.List
+import chapter3.foldLeft
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun sumL(xs: List<Int>): Int =
-
-    SOLUTION_HERE()
+fun sumL(xs: List<Int>): Int = foldLeft(xs, 0) { x, y -> x + y }
 
 fun productL(xs: List<Double>): Double =
+    foldLeft(xs, 1.0) { x, y -> x * y }
 
-    SOLUTION_HERE()
-
-fun <A> lengthL(xs: List<A>): Int =
-
-    SOLUTION_HERE()
+fun <A> lengthL(xs: List<A>): Int = foldLeft(xs, 0) { s, _ -> s + 1 }
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise10 : WordSpec({
-    "!list sumL" should {
+    "list sumL" should {
         "add all integers" {
             sumL(List.of(1, 2, 3, 4, 5)) shouldBe 15
         }
     }
 
-    "!list productL" should {
+    "list productL" should {
         "multiply all doubles" {
             productL(List.of(1.0, 2.0, 3.0, 4.0, 5.0)) shouldBe
                 120.0
         }
     }
 
-    "!list lengthL" should {
+    "list lengthL" should {
         "count the list elements" {
             lengthL(List.of(1, 2, 3, 4, 5)) shouldBe 5
         }
