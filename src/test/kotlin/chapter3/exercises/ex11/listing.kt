c97170dd83defdb882068e3fd102d391a2518b2b
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 16:19:43 +0100

    Exercise 3.11

--- src/test/kotlin/chapter3/exercises/ex11/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex11/listing.kt
@@ -1,20 +1,20 @@
 package chapter3.exercises.ex11
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldLeft
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> reverse(xs: List<A>): List<A> =
-
-    SOLUTION_HERE()
+    foldLeft(xs, List.empty()) { l, a -> Cons(a, l) }
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise11 : WordSpec({
     "list reverse" should {
-        "!reverse list elements" {
+        "reverse list elements" {
             reverse(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(5, 4, 3, 2, 1)
         }
