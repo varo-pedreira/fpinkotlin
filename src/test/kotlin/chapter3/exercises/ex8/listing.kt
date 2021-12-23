43684a08ad8c1b38c7ccbd6ad3f615770cca8fc1
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 14:33:43 +0100

    Exercise 3.8

--- src/test/kotlin/chapter3/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex8/listing.kt
@@ -2,24 +2,22 @@ package chapter3.exercises.ex8
 
 import chapter3.List
 import chapter3.Nil
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun <A> length(xs: List<A>): Int =
-
-    SOLUTION_HERE()
+fun <A> length(xs: List<A>): Int = foldRight(xs, 0) { _, b -> b + 1 }
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise8 : WordSpec({
     "list length" should {
-        "!calculate the length" {
+        "calculate the length" {
             length(List.of(1, 2, 3, 4, 5)) shouldBe 5
         }
 
-        "!calculate zero for an empty list" {
+        "calculate zero for an empty list" {
             length(Nil) shouldBe 0
         }
     }
