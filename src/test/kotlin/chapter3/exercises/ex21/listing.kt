cba57a93a37b23fd0bd07c24db5d9f10d08a5729
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 18:04:51 +0100

    Exercise 3.21

--- src/test/kotlin/chapter3/exercises/ex21/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex21/listing.kt
@@ -1,20 +1,38 @@
 package chapter3.exercises.ex21
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
+import chapter3.append
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun add(xa: List<Int>, xb: List<Int>): List<Int> =
-
-    SOLUTION_HERE()
+fun add(xa: List<Int>, xb: List<Int>): List<Int> {
+    tailrec fun go(
+        first: List<Int>,
+        second: List<Int>,
+        acc: List<Int>
+    ): List<Int> =
+        when (first) {
+            is Nil -> acc
+            is Cons -> when (second) {
+                is Nil -> acc
+                is Cons -> go(
+                    acc = append(acc, List.of(first.head + second.head)),
+                    first = first.tail,
+                    second = second.tail
+                )
+            }
+        }
+    return go(first = xa, second = xb, acc = List.empty())
+}
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise21 : WordSpec({
     "list add" should {
-        "!add elements of two corresponding lists" {
+        "add elements of two corresponding lists" {
             add(List.of(1, 2, 3), List.of(4, 5, 6)) shouldBe
                 List.of(5, 7, 9)
         }
