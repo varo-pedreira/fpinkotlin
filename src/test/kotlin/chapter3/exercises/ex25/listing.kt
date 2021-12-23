c0e0f730a380adab29c59150252a3d8c4219d72b
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 21:12:14 +0100

    Exercise 3.25

--- src/test/kotlin/chapter3/exercises/ex25/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex25/listing.kt
@@ -5,18 +5,21 @@ import chapter3.Leaf
 import chapter3.Tree
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun maximum(tree: Tree<Int>): Int =
-
-    SOLUTION_HERE()
+fun maximum(tree: Tree<Int>): Int {
+    fun find(tree: Tree<Int>, acc: Int): Int = when (tree) {
+        is Branch -> maxOf(find(tree.left, acc), find(tree.right, acc))
+        is Leaf -> maxOf(acc, tree.value)
+    }
+    return find(tree, 0)
+}
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise25 : WordSpec({
     "tree maximum" should {
-        "!determine the maximum value held in a tree" {
+        "determine the maximum value held in a tree" {
             val tree = Branch(
                 Branch(Leaf(1), Leaf(9)),
                 Branch(Leaf(3), Leaf(4))
