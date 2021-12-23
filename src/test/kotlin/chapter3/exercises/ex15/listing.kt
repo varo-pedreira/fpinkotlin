b2d29dc18123ea507fd3ff71782c45bc729669a3
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 17:05:12 +0100

    Exercise 3.15

--- src/test/kotlin/chapter3/exercises/ex15/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex15/listing.kt
@@ -1,20 +1,20 @@
 package chapter3.exercises.ex15
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun increment(xs: List<Int>): List<Int> =
-
-    SOLUTION_HERE()
+    foldRight(xs, List.empty()) { a, ls -> Cons(a + 1, ls) }
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise15 : WordSpec({
     "list increment" should {
-        "!add 1 to every element" {
+        "add 1 to every element" {
             increment(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(2, 3, 4, 5, 6)
         }
