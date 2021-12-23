a359eed7f266e49701b6cecbdedf7c4b7b19521a
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-23 11:56:22 +0100

    Exercise 3.3

--- src/test/kotlin/chapter3/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex3/listing.kt
@@ -1,31 +1,35 @@
 package chapter3.exercises.ex3
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun <A> drop(l: List<A>, n: Int): List<A> =
-
-    SOLUTION_HERE()
+fun <A> drop(l: List<A>, n: Int): List<A> {
+    tailrec fun go(xs: List<A>, i: Int): List<A> = when (xs) {
+        is Cons -> if (i == 1) xs.tail else go(xs.tail, i - 1)
+        Nil -> throw IllegalStateException()
+    }
+    return go(l, n)
+}
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise3 : WordSpec({
     "list drop" should {
-        "!drop a given number of elements within capacity" {
+        "drop a given number of elements within capacity" {
             drop(List.of(1, 2, 3, 4, 5), 3) shouldBe
                 List.of(4, 5)
         }
 
-        "!drop a given number of elements up to capacity" {
+        "drop a given number of elements up to capacity" {
             drop(List.of(1, 2, 3, 4, 5), 5) shouldBe Nil
         }
 
-        """!throw an illegal state exception when dropped elements
+        """throw an illegal state exception when dropped elements
             exceed capacity""" {
             shouldThrow<IllegalStateException> {
                 drop(List.of(1, 2, 3, 4, 5), 6)
