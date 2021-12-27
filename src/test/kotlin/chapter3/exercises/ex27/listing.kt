ef0f546fd1a1bc098815ae5ec890987ccd6ea0d5
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-27 17:30:58 +0100

    Exercise 4.1 and 3.27

--- src/test/kotlin/chapter3/exercises/ex27/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex27/listing.kt
@@ -5,17 +5,22 @@ import chapter3.Leaf
 import chapter3.Tree
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A, B> map(tree: Tree<A>, f: (A) -> B): Tree<B> =
+    when (tree) {
+        is Leaf -> Leaf(f(tree.value))
+        is Branch -> Branch(
+            right = map(tree.right, f),
+            left = map(tree.left, f)
+        )
+    }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise27 : WordSpec({
-    "!tree map" should {
+    "tree map" should {
         "transform all leaves of a map" {
             val actual = Branch(
                 Branch(Leaf(1), Leaf(2)),
--- src/test/kotlin/chapter4/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex1/listing.kt
@@ -9,45 +9,24 @@ import utils.SOLUTION_HERE
 
 //tag::init[]
 fun <A, B> Option<A>.map(f: (A) -> B): Option<B> =
-
-    SOLUTION_HERE() // <1>
+    if (this is Some) Some(f(this.get)) else None
 
 fun <A, B> Option<A>.flatMap(f: (A) -> Option<B>): Option<B> =
-
-    SOLUTION_HERE()// <2>
+    if (this is Some) f(this.get) else None
 
 fun <A> Option<A>.getOrElse(default: () -> A): A =
-
-    SOLUTION_HERE() // <3>
+    if (this is Some) this.get else default()
 
 fun <A> Option<A>.orElse(ob: () -> Option<A>): Option<A> =
-
-    SOLUTION_HERE() // <4>
+    if (this is Some) this else ob()
 
 fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> =
-
-    SOLUTION_HERE() // <5>
+    when (this) {
+        None -> None
+        is Some -> if (f(this.get)) this else None
+    }
 //end::init[]
 
-//tag::alternate[]
-fun <A, B> Option<A>.flatMap_2(
-    f: (A) -> Option<B>
-): Option<B> =
-
-    SOLUTION_HERE()
-
-fun <A> Option<A>.orElse_2(
-    ob: () -> Option<A>
-): Option<A> =
-
-    SOLUTION_HERE()
-
-fun <A> Option<A>.filter_2(
-    f: (A) -> Boolean
-): Option<A> =
-
-    SOLUTION_HERE()
-//end::alternate[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
@@ -57,64 +36,54 @@ class Exercise1 : WordSpec({
     val some = Some(10)
 
     "option map" should {
-        "!transform an option of some value" {
+        "transform an option of some value" {
             some.map { it * 2 } shouldBe Some(20)
         }
-        "!pass over an option of none" {
+        "pass over an option of none" {
             none.map { it * 10 } shouldBe None
         }
     }
 
     "option flatMap" should {
-        """!apply a function yielding an option to an
+        """apply a function yielding an option to an
             option of some value""" {
             some.flatMap { a ->
                 Some(a.toString())
             } shouldBe Some("10")
 
-            some.flatMap_2 { a ->
-                Some(a.toString())
-            } shouldBe Some("10")
         }
-        "!pass over an option of none" {
+        "pass over an option of none" {
             none.flatMap { a ->
                 Some(a.toString())
             } shouldBe None
 
-            none.flatMap_2 { a ->
-                Some(a.toString())
-            } shouldBe None
         }
     }
 
     "option getOrElse" should {
-        "!extract the value of some option" {
+        "extract the value of some option" {
             some.getOrElse { 0 } shouldBe 10
         }
-        "!return a default value if the option is none" {
+        "return a default value if the option is none" {
             none.getOrElse { 10 } shouldBe 10
         }
     }
 
     "option orElse" should {
-        "!return the option if the option is some" {
+        "return the option if the option is some" {
             some.orElse { Some(20) } shouldBe some
-            some.orElse_2 { Some(20) } shouldBe some
         }
-        "!return a default option if the option is none" {
+        "return a default option if the option is none" {
             none.orElse { Some(20) } shouldBe Some(20)
-            none.orElse_2 { Some(20) } shouldBe Some(20)
         }
     }
 
     "option filter" should {
-        "!return some option if the predicate is met" {
+        "return some option if the predicate is met" {
             some.filter { it > 0 } shouldBe some
-            some.filter_2 { it > 0 } shouldBe some
         }
-        "!return a none option if the predicate is not met" {
+        "return a none option if the predicate is not met" {
             some.filter { it < 0 } shouldBe None
-            some.filter_2 { it < 0 } shouldBe None
         }
     }
 })
