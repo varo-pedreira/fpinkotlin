999a1746cc1660b4d4a8575792f446878a0c43e0
alvaropedreiraescribano <alvaro.pedreira@code.seat>
2021-12-27 18:51:45 +0100

    Exercise 4.3

--- src/test/kotlin/chapter4/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex3/listing.kt
@@ -5,12 +5,11 @@ import chapter4.Option
 import chapter4.Some
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //tag::init[]
 fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
+    a.flatMap { x -> b.map { y -> f(x, y) } }
 
-    SOLUTION_HERE()
 //end::init[]
 
 //TODO: Enable tests by removing `!` prefix
@@ -22,13 +21,13 @@ class Exercise3 : WordSpec({
         val b = Some(20)
         val none = Option.empty<Int>()
 
-        "!combine two option values using a binary function" {
+        "combine two option values using a binary function" {
             map2(a, b) { aa, bb ->
                 aa * bb
             } shouldBe Some(100)
         }
 
-        "!return none if either option is not defined" {
+        "return none if either option is not defined" {
             map2(a, none) { aa, bb ->
                 aa * bb
             } shouldBe None
